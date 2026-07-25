0aef36934e322943c80ca8ba39945b4529257f49
Gianluca Panetta <g.panetta@outlook.de>
2026-07-25 20:31:06 +0200

    somehow i needed the previous change before, but now it's running with the original content of this file again

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { sveltePhosphorOptimize } from 'phosphor-svelte/vite';
 
 export default defineConfig({
-	server: {
-		watch: {
-			ignored: [
-				"**/src-tauri/target/**",
-				"**/target/**",
-			],
-		}
-  	},
 	plugins: [tailwindcss(), sveltekit(), sveltePhosphorOptimize(), devtoolsJson()],
 	test: {
 		expect: { requireAssertions: true },
